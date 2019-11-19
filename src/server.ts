61e89530dde6ccb0301d7d0de33ac6acf8a7695d
xiaosongfu <xiaosong.fu@outlook.com>
2019-11-19 10:22:30 +0800

    add heroku deploy file

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const server = http.createServer((req: IncomingMessage, res: ServerResponse) => 
   res.end()
 });
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 9090
+const port = process.env.PORT || 9090
 
 server.listen(port, () => {
   console.log(`server is listening on localhost: ${port}`)
