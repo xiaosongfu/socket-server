de2a197006011aad31c246fba8cd9abf7b5fb23e
xiaosongfu <xiaosong.fu@outlook.com>
2019-11-20 00:26:11 +0800

    add socket.io and done chat func

---
+++ install.md
@@ -0,0 +1,13 @@
+
+```
+npm i socket.io
+npm i @types/socket.io -D
+
+npm i pino pino-http
+npm i @types/pino @types/pino-http -D
+```
+
+* https://segmentfault.com/a/1190000019453527
+* https://github.com/pinojs/pino
+* https://github.com/pinojs/pino-http
+* https://github.com/pinojs/pino/blob/master/docs/api.md
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,64 @@
 import http, {IncomingMessage, ServerResponse} from 'http'
+import socketIO from 'socket.io'
+import pino from 'pino'
+import pinoHttp from 'pino-http'
+
+const logger = pino({
+  level: "debug",
+})
+const loggerHttp = pinoHttp({
+  logger: logger,
+})
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  loggerHttp(req, res)
+  req.log.info('pino-http-test')
+
   res.write('hello world')
   res.end()
 });
 
-const port = process.env.PORT || 9090
+// 给本连接发送消息： socket.emit()
+// 给某个房间内的所有人发送消息： io.in(room).emit()
+// 除本连接外，给某个房间内所有人发送消息：socket.to(room).emit()
+// 除本连接外，给所有人发送消息：socket.broadcast.emit()
+const io = socketIO.listen(server)
+io.on('connection', socket => {
+  logger.debug(`${socket.id} connect!`)
 
+  // disconnect 是内置事件
+  socket.on('disconnect', args => {
+    logger.debug(`${socket.id} disconnect!`)
+  })
+
+  // -----------------------------------------
+  // -----------------------------------------
+
+  // join 是自定义事件
+  socket.on('join', (roomName, userName) => {
+    // 进入房间
+    socket.join(roomName)
+
+    // 发广播通知用户加入
+    socket.to(roomName).emit('joined', roomName, userName)
+    logger.debug(`${socket.id} joined!`)
+  })
+  // leave 是自定义事件
+  socket.on('leave', (roomName, userName) => {
+    // 退出房间
+    socket.leave(roomName)
+
+    // 发广播通知用户离开
+    socket.to(roomName).emit('leaved', roomName, userName)
+    logger.debug(`${socket.id} leaved!`)
+  })
+  // message 是自定义事件
+  socket.on('message', (roomName, userName, msg) => {
+    socket.to(roomName).emit('message', roomName, userName, msg)
+  })
+})
+
+const port = process.env.PORT || 9090
 server.listen(port, () => {
-  console.log(`server is listening on localhost: ${port}`)
+  logger.debug(`server is listening on localhost: ${port}`)
 })
